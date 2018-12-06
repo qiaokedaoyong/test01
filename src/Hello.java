945b0f72ba0425b29ef9450fdb6a0549fd737fb1
qiaokedaoyong <2417988509@qq.com>
2018-12-06 15:38:46 +0800

    Hello注视２２２２

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@
  * Created by hand on 2018/12/6.
  */
 public class Hello {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        System.out.println("hahahhaha");
     }
 }

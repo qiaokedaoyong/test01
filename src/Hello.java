757dcb5b3ce28ba2a69f07d0c0c4e08f622560cc
qiaokedaoyong <2417988509@qq.com>
2018-12-06 16:28:10 +0800

    Hello注视２２２２

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("ｃｅｓｈｉ");
         System.out.println("冯勇");
+        System.out.println("冯勇master");
     }
 }
